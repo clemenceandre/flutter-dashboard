9515642800b0ee61192b0559cafb090e08adc08a
noeliecomte <noelie.comte02@gmail.com>
2024-11-11 23:24:51 +0100

    Revert "modification de la selection de dates"
    This reverts commit 11d64442818c1767a95a1a53682af90f473588bb.

--- lib/main.dart
+++ lib/main.dart
@@ -39,49 +39,51 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
   bool _isLoading = true;
   Map<String, dynamic>? _cheapestFlightDetails;
 
-  DateTime? _selectedDate; // Remplace _selectedYear et _selectedMonth
+  // Variables pour l'année et le mois sélectionnés
+  int _selectedYear = DateTime.now().year;
+  String _selectedMonth = '';
+  final List<String> months = [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ];
+
+  DateTime _getStartDate(String month, int year) {
+    int monthIndex = months.indexOf(month) + 1;
+    return DateTime(year, monthIndex, 1);
+  }
+
+  DateTime _getEndDate(String month, int year) {
+    int monthIndex = months.indexOf(month) + 1;
+    return DateTime(year, monthIndex + 1, 0); // Dernier jour du mois
+  }
 
   @override
   void initState() {
     super.initState();
-    _selectedDate = DateTime.now(); // Date par défaut (aujourd'hui)
-    _getFlightInspiration(); // Appel de l'API avec la date sélectionnée
+    _selectedYear = DateTime.now().year; // Année actuelle
+    _selectedMonth = months[DateTime.now().month]; // Mois prochain
+    _getFlightInspiration(); // Appelle la méthode lors de l'initialisation
+    _getOtherDates();
   }
 
-  // Méthode pour ouvrir un sélecteur de date
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime currentDate = DateTime.now();
-    final DateTime maxDate = DateTime(
-      currentDate.year,
-      currentDate.month + 6,
-      currentDate.day,
-    ); // Limite de sélection jusqu'à 6 mois après aujourd'hui
-
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate ?? currentDate,
-      firstDate: currentDate, // Restriction pour les dates antérieures à aujourd'hui
-      lastDate: maxDate, // Restriction jusqu'à 6 mois dans le futur
-    );
-
-    if (pickedDate != null && pickedDate != _selectedDate) {
-      setState(() {
-        _selectedDate = pickedDate;
-        _isLoading = true;
-      });
-      _getFlightInspiration(); // Mise à jour des inspirations de vol
-    }
-  }
-
-  // Méthode pour obtenir les inspirations de vol (mise à jour pour utiliser _selectedDate)
+  // Méthode pour obtenir les inspirations de vol
   void _getFlightInspiration() async {
-    final year = _selectedDate?.year;
-    final month = _selectedDate?.month ?? 1;
     String departureDate =
-        '${DateTime(year!, month, 1).toIso8601String().split('T')[0]},${DateTime(year, month + 1, 0).toIso8601String().split('T')[0]}';
+        '${_getStartDate(_selectedMonth, _selectedYear).toIso8601String().split('T')[0]},${_getEndDate(_selectedMonth, _selectedYear).toIso8601String().split('T')[0]}';
 
     try {
-      _destinations = await _amadeusApi.getFlightInspiration('MAD', departureDate);
+      _destinations =
+          await _amadeusApi.getFlightInspiration('MAD', departureDate);
       setState(() {
         _isLoading = false;
       });
@@ -164,7 +166,7 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
       // Appeler la méthode pour obtenir les autres dates
       _getOtherDates().then((datesList) {
         final matchedDate = datesList.firstWhere(
-              (date) => date['destination'] == destination['destination'],
+          (date) => date['destination'] == destination['destination'],
           orElse: () => {
             'destination': destination['destination'],
             'prix': 'N/A',
@@ -172,6 +174,16 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
             'retourDate': 'N/A'
           },
         );
+
+        // Assurez-vous que 'prix' est une valeur double, sinon assigner une valeur par défaut
+        double price = matchedDate['prix'] != 'N/A'
+            ? double.tryParse(matchedDate['prix'].toString()) ??
+                0.0 // Si la conversion échoue, utilisez 0.0
+            : 0.0;
+
+        double minPrice = 50.0; // Prix minimum pour l'échelle (par exemple, 50)
+        double maxPrice =
+            1000.0; // Prix maximum pour l'échelle (par exemple, 1000)
 
         showDialog(
           context: context,
@@ -205,7 +217,8 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           Text(
-                            destination['destination'] ?? 'Destination inconnue',
+                            destination['destination'] ??
+                                'Destination inconnue',
                             style: const TextStyle(
                                 fontSize: 20, fontWeight: FontWeight.bold),
                           ),
@@ -225,10 +238,60 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
                                             fontWeight: FontWeight.bold)),
                                     Text(
                                         'Prix: ${_cheapestFlightDetails!['price']['total']} ${_cheapestFlightDetails!['price']['currency']}'),
+                                    Text(
+                                        'Durée: ${_cheapestFlightDetails!['itineraries'][0]['duration']}'),
+                                    Text(
+                                        'Départ: ${_cheapestFlightDetails!['itineraries'][0]['segments'][0]['departure']['iataCode']} à ${_cheapestFlightDetails!['itineraries'][0]['segments'][0]['departure']['at']}'),
+                                    Text(
+                                        'Arrivée: ${_cheapestFlightDetails!['itineraries'][0]['segments'][0]['arrival']['iataCode']} à ${_cheapestFlightDetails!['itineraries'][0]['segments'][0]['arrival']['at']}'),
+                                    Text(
+                                        'Compagnie: ${_cheapestFlightDetails!['validatingAirlineCodes'][0]}'),
                                   ],
                                 ),
                               ),
                             ),
+                          ],
+                          // Afficher les détails des autres dates si une correspondance est trouvée
+                          if (matchedDate['prix'] != 'N/A') ...[
+                            const SizedBox(height: 20),
+                            Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: [
+                                    const Text(
+                                        'Le prix le moins cher pour cette destination',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold)),
+                                    Text('Prix: ${matchedDate['prix']}'),
+                                    Text(
+                                        'Date Aller: ${matchedDate['allerDate']}'),
+                                    Text(
+                                        'Date Retour: ${matchedDate['retourDate']}'),
+                                    const SizedBox(height: 20),
+                                    // Afficher un Slider de couleur en fonction du prix
+                                    Slider(
+                                      value: price,
+                                      min: minPrice,
+                                      max: maxPrice,
+                                      onChanged: (value) {},
+                                      activeColor: _getPriceColor(
+                                          price, minPrice, maxPrice),
+                                      inactiveColor: Colors.grey,
+                                    ),
+                                    Text(
+                                        'Position du prix: ${price.toString()}'),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ] else ...[
+                            // Afficher un message si aucun prix n'est trouvé
+                            const SizedBox(height: 20),
+                            const Text(
+                                'Pas de meilleur prix disponible pour cette destination.',
+                                style: TextStyle(fontSize: 16)),
                           ],
                         ],
                       ),
@@ -243,6 +306,16 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
     });
   }
 
+  Color _getPriceColor(double price, double minPrice, double maxPrice) {
+    if (price <= minPrice) {
+      return Colors.green; // Prix faible (vert)
+    } else if (price >= maxPrice) {
+      return Colors.red; // Prix élevé (rouge)
+    } else {
+      return Colors.yellow; // Prix moyen (jaune)
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -251,72 +324,140 @@ class _FlightInspirationScreenState extends State<FlightInspirationScreen> {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () => _selectDate(context), // Ouvre le calendrier
-              child: Text(
-                _selectedDate != null
-                    ? 'Date sélectionnée : ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
-                    : 'Sélectionner une date',
+          // Liste déroulante pour l'année
+          DropdownButton<int>(
+            value: _selectedYear,
+            items: List.generate(
+              2,
+              (index) => DropdownMenuItem(
+                value: DateTime.now().year + index,
+                child: Text('${DateTime.now().year + index}'),
               ),
             ),
+            onChanged: (value) {
+              setState(() {
+                _selectedYear = value!;
+              });
+            },
           ),
+          // Boutons pour les mois
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: months.map((month) {
+              return ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedMonth = month;
+                    _isLoading = true;
+                  });
+                  _getFlightInspiration(); // Lancer la requête API
+                },
+                child: Text(month),
+              );
+            }).toList(),
+          ),
+          // Affichage de la carte ou du chargement
           _isLoading
               ? const Expanded(child: Center(child: CircularProgressIndicator()))
               : Expanded(
-            child: MapPage(
-              destinations: _destinations,
-              showDestinationDetails: _showDestinationDetails,
-            ),
-          ),
+                  child: MapPage(
+                      destinations: _destinations,
+                      showDestinationDetails: _showDestinationDetails)),
         ],
       ),
     );
   }
 }
 
-// Widget MapPage pour afficher la carte
 class MapPage extends StatelessWidget {
   final List<Map<String, dynamic>> destinations;
-  final Function(BuildContext, Map<String, dynamic>) showDestinationDetails;
+  final Function(BuildContext, Map<String, dynamic>)
+      showDestinationDetails; // Change ici
 
-  const MapPage({
-    Key? key,
-    required this.destinations,
-    required this.showDestinationDetails,
-  }) : super(key: key);
+  const MapPage({super.key, required this.destinations, required this.showDestinationDetails});
+
+  Color _getColorForPrice(Map<String, dynamic> destination,
+      List<Map<String, dynamic>> destinations) {
+    double minPrice = double.infinity;
+    double maxPrice = 0.0;
+
+    // Trouver les prix minimum et maximum
+    for (var destination in destinations) {
+      double prix = double.tryParse(destination['prix'] ?? '0') ?? 0.0;
+      if (prix > 0) {
+        if (prix < minPrice) minPrice = prix;
+        if (prix > maxPrice) maxPrice = prix;
+      }
+    }
+    double currentPrice = double.tryParse(destination['prix'] ?? '0') ?? 0.0;
+
+    // Si le prix est le minimum, retourner vert
+    if (currentPrice == minPrice) {
+      return Colors.green; // Le moins cher en vert
+    }
+    // Si le prix est le maximum, retourner rouge
+    else if (currentPrice == maxPrice) {
+      return Colors.red; // Le plus cher en rouge
+    }
+    // Pour les autres prix, interpoler la couleur entre vert et rouge
+    else {
+      double normalizedPrice =
+          (currentPrice - minPrice) / (maxPrice - minPrice);
+      return Color.lerp(Colors.green, Colors.red, normalizedPrice) ??
+          Colors.red;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(40.4168, -3.7038), // Centre sur Madrid par défaut
-        zoom: 3,
+        center: LatLng(20.0, 0.0),
+        zoom: 2.0,
       ),
       children: [
         TileLayer(
           urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: ['a', 'b', 'c'],
+          subdomains: const ['a', 'b', 'c'],
         ),
         MarkerLayer(
           markers: destinations.map((destination) {
-            final lat = destination['latitude'] as double?;
-            final lon = destination['longitude'] as double?;
-            if (lat == null || lon == null) return null;
+            final latitude = destination['latitude'];
+            final longitude = destination['longitude'];
 
             return Marker(
-              point: LatLng(lat, lon),
-              builder: (ctx) => GestureDetector(
-                onTap: () => showDestinationDetails(context, destination),
-                child: const Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                  size: 30,
+              width: 30.0, // Taille de l'ombre
+              height: 30.0, // Taille de l'ombre
+              point: LatLng(latitude, longitude),
+              builder: (ctx) => Container(
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onTap: () {
+                    showDestinationDetails(
+                        context, destination); // Afficher les détails sur clic
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Ombre
+                      CircleAvatar(
+                        radius: 15, // Rayon de l'ombre
+                        backgroundColor:
+                            Colors.grey.withOpacity(0.5), // Couleur de l'ombre
+                      ),
+                      // Pin
+                      Icon(
+                        Icons.location_on,
+                        color: _getColorForPrice(destination, destinations),
+                        size: 25, // Taille de l'icône
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
-          }).whereType<Marker>().toList(),
+          }).toList(),
         ),
       ],
     );
